d3e3914c725cd1c263e8349a56a2483e0f19131a
Ruari Day <ruariday@gmail.com>
2020-03-04 15:56:13 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase == phrase
     return "I LOVE YOU TOO PUMPKIN!"
-  
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
